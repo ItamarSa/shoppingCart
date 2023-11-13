b21919376dbe0e4ba7c6cbf5ae02cb85e3bcdaee
yinonab <yinonabarjel@gmail.com>
2023-11-13 14:53:12 +0200

    F/add list=preview cmps

---
+++ src/cmps/ProductList.jsx
@@ -0,0 +1,28 @@
+import { ProductPreview } from "./ProductPreview";
+
+export function ProductList(onAddToCart,products){
+return(
+    <>  
+    <div className="list">
+        <div className="amount">product available{product.length}
+
+        </div>
+
+    </div>
+    <ul className='product-list'>
+                {products.map(product =>
+                    <li className='product-preview' key={product._id}>
+                        <ProductPreview
+                            product={product}
+                            onAddToCart={onAddToCart}
+                        />
+                    </li>)
+                }
+            </ul>
+    </>
+)
+
+
+
+
+}
