976eeba49625eb2d1b0b5262e4e7e48f0a21d53f
itamarsa <itamarr87@gmail.com>
2023-11-13 16:19:35 +0200

    F/setup-hompage/add to cart

--- src/cmps/ProductPreview.jsx
+++ src/cmps/ProductPreview.jsx
@@ -9,7 +9,7 @@ export function ProductPreview(onAddToCart, product) {
                     <p className="product-title">{product.title}</p>
                     <p className="product-price">{product.price}</p>
                 </div>
-                <button className="cart-btn" onClick={onAddToCart()}></button>
+                <button className="cart-btn" onClick={() => onAddToCart}></button>
 
 
             </div>
